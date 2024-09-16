a4e99c7bb64b75fd7e269d3c082b1be283f92391
ryan.burleigh@payprop.co.uk <ryan.burleigh@payprop.co.uk>
2024-09-16 12:35:56 +0000

    Edited ProjectDetailForm_copy.RowTemplate2_copy and ProjectDetailForm_copy

--- client_code/ProjectDetailForm_copy/__init__.py
+++ client_code/ProjectDetailForm_copy/__init__.py
@@ -7,24 +7,24 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class ProjectDetailForm_copy(ProjectDetailForm_copyTemplate):
-  def __init__(self, project=None, **properties):
+#class ProjectDetailForm_copy(ProjectDetailForm_copyTemplate):
+ # def __init__(self, project=None, **properties):
     # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+#    self.init_components(**properties)
 
     # Save the passed project row to use later
-    self.project_row = project  # Use the 'project' parameter passed to the form
+ #   self.project_row = project  # Use the 'project' parameter passed to the form
 
     # Load tasks for the project into the data grid
-    self.load_tasks()
+#    self.load_tasks()
 
-  def load_tasks(self):
+ # def load_tasks(self):
     # Ensure we have a valid project_row
-    if self.project_row:
+#    if self.project_row:
       # Call the server function to get tasks for this project
-      tasks = anvil.server.call("get_tasks_for_project", self.project_row)
+  #    tasks = anvil.server.call("get_tasks_for_project", self.project_row)
 
       # Set the tasks to the data grid or repeating panel
-      self.repeating_panel_1.items = tasks
-    else:
-      alert("No project was provided to load tasks.")
+  #    self.repeating_panel_1.items = tasks
+ #   else:
+    #  alert("No project was provided to load tasks.")
