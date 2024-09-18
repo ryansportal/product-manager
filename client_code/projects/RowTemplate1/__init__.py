6e837cbc4b80024bfd2c4aeb80b3a6e29aa39d00
ryan.burleigh@payprop.co.uk <ryan.burleigh@payprop.co.uk>
2024-09-18 10:50:34 +0000

    Edited projects.RowTemplate1

--- client_code/projects/RowTemplate1/__init__.py
+++ client_code/projects/RowTemplate1/__init__.py
@@ -27,6 +27,12 @@ class RowTemplate1(RowTemplate1Template):
     
   def text_box_project_description_show(self, **event_args):
     self.text_box_project_description.text = str(self.item['description'])
+    
+  def assignee_drop_down_show(self, **event_args):
+   # If there is an assigned user, set the selected_value to the user row
+        if self.item['assignee']:
+            self.assignee_drop_down.selected_value = self.item['assignee']
+
 
  
 
@@ -35,7 +41,9 @@ class RowTemplate1(RowTemplate1Template):
         # self.item refers to the current project's data (automatically set by the repeating panel)
     self.text_box_1.text = str(self.item['project_name'])
     self.project_description_label.text = str(self.item['description'])
-
+        # Set the selected_value of the dropdown based on the assignee (user row)
+    if self.item['assignee']:
+            self.assignee_drop_down.selected_value = self.item['assignee']
 
 
 
@@ -55,7 +63,8 @@ class RowTemplate1(RowTemplate1Template):
                 anvil.server.call('update_project',
                                   updated_project['id'],
                                   updated_project.get('project_name', ''),
-                                  updated_project.get('description', '')
+                                  updated_project.get('description', ''),
+                                  updated_project.get('assignee', '') 
                                  )
         except Exception as e:
             print(f"Error during auto-save: {e}")
@@ -72,6 +81,11 @@ class RowTemplate1(RowTemplate1Template):
             self.item['description'] = self.text_box_project_description.text
             self.auto_save()
 
+  def assignee_drop_down_change(self, **event_args):
+   if self.item:
+            selected_user_row = self.assignee_drop_down.selected_value  # This will be the user row
+            self.item['assignee'] = selected_user_row  # Set the assignee as the user row
+            self.auto_save()
 
 
   
